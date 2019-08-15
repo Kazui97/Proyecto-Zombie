c9b6bfa9f9cb6a6e8ba1a290a02bae2d7a2c06b3
Kazui97 <camyescobar12@gmail.com>
2019-08-15 17:50:17 -0500

    proyecto-zombie-1
    la

---
+++ Assets/Generador.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generador : MonoBehaviour
+{
+    GameObject ZombieMesh;
+    CosasZombie datoZombi;
+    
+
+    void Start()
+    {
+
+
+
+        int numZombi = Random.Range(9, 20);
+        for (int i = 0; i < numZombi; i++)
+        {
+            ZombieMesh = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+            ZombieMesh.AddComponent<ZombieOp>();
+            datoZombi = ZombieMesh.GetComponent<ZombieOp>().datosZombi;
+            switch (datoZombi.colorEs)
+            {
+                case CosasZombie.ColorZombie.magenta:
+                    ZombieMesh.GetComponent<Renderer>().material.color = Color.magenta;
+
+                    break;
+                case CosasZombie.ColorZombie.green:
+                    ZombieMesh.GetComponent<Renderer>().material.color = Color.green;
+
+                    break;
+                case CosasZombie.ColorZombie.cyan:
+                    ZombieMesh.GetComponent<Renderer>().material.color = Color.cyan;
+
+                    break;
+            }
+            Vector3 pos = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+            ZombieMesh.transform.position = pos;
+            ZombieMesh.AddComponent<Rigidbody>();
+
+        }
+       
+
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ZombieOp.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieOp : MonoBehaviour
+{
+    
+
+    // public string [] Gustos;
+    public CosasZombie datosZombi;
+
+
+
+    void Awake()
+    {
+
+
+
+
+        //ZombieMesh = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        //Vector3 pos = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+        //ZombieMesh.transform.position = pos;
+        //ZombieMesh.AddComponent<Rigidbody>();
+
+
+        datosZombi.colorEs = (CosasZombie.ColorZombie)Random.Range(0, 3);
+
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+   
+
+
+}
+
+
+
+public struct CosasZombie
+{
+    public enum Gustos 
+    {
+        Brazos,
+        Piernas,
+        Huesitos,
+        Ojito,
+        corazoncito
+    };
+    public Gustos sabroso;
+
+    public enum Estados
+    {
+        Idle,
+        Moving
+    };
+    public Estados condicion;
+
+    public enum ColorZombie
+    {
+        magenta,
+        green, 
+        cyan
+    };
+    public ColorZombie colorEs;
+}
